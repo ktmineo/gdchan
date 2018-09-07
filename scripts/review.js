e50eeb099d826d787e3c97a8e7519b3973a5b7e7
ST20900 <st20900@ismac-PC-a860b61d6b2e.local>
2018-09-07 15:54:19 +0900

    coffee

--- scripts/review.js
+++ scripts/review.js
@@ -1,35 +1,34 @@
 import request from 'request';
 
-module.exports = function (robot) {
-	robot.respond(/レビュー/i, function (msg) {
-		const url = 'https://slack.com/api/channels.list?token=' + process.env.HUBOT_SLACK_TOKEN;
+module.exports = (robot) => {
+  robot.respond(/.*(random|抽選|選ぶ).*/, (msg) => {
+    const url = 'https://slack.com/api/channels.list?token=' + process.env.HUBOT_SLACK_TOKEN;
 
-		//ch 一覧取得
-		request(url, (err, res, body){
-			const channel = findChannel(JSON.parse(body).channels, msg.message.room);
-			console.log('get ch');
-			console.log(channel);
+    // チャンネル一覧を取得
+    request(url, (err, res, body) => {
+      // msg.message.room で現在の channel 名が取れる
+      const channel = findChannel(JSON.parse(body).channels, msg.message.room);
+      console.log('Channel found');
+      console.log(channel);
 
-			//bot以外で抽選
-			const botId = robot.adapter.self.id;
-			const filterdMembers = channel.members.filter ((member) {
-				return member !== botId;
-			});
-			consol.log(filterdMembers);
+      // bot 自身を除外して抽選
+      const botId = robot.adapter.self.id;
+      const filterdMembers = channel.members.filter((member) => {
+        return member !== botId;
+      });
+      console.log(filterdMembers);
+      const member = msg.random(filterdMembers);
 
-			const member = msg.random(filterdMembers);
-
-			//送信
-			msg.send('<@' + member + '> レビューお願いします。 :tea:');
-		});
-	});
-｝
+      msg.send('選ばれたのは、 <@' + member + '> でした :tea:');
+    });
+  });
+}
 
 function findChannel(channels, targetName) {
-	for (const channel of channels) {
-		if (channel.name === targetName) {
-			return channel;
-		}
-	}
-	return null;
+  for (const channel of channels) {
+    if (channel.name === targetName) {
+      return channel;
+    }
+  }
+  return null;
 };
